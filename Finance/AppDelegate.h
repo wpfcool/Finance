a47460f8cc9f882f6c0a32b77fabc35e6b501895
wenpeifang <wenpeifang@jd.com>
2018-06-04 11:24:07 +0800

    Initial Commit

---
+++ Finance/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Finance
+//
+//  Created by wenpeifang on 2018/6/4.
+//  Copyright © 2018年 wenpeifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Finance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Finance
+//
+//  Created by wenpeifang on 2018/6/4.
+//  Copyright © 2018年 wenpeifang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
